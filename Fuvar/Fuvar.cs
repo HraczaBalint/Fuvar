bc6cb50c4cbce46dac3993c879972f528e4ffa17
HraczaBalint <hraczabalintmail@gmail.com>
2021-03-12 17:52:28 +0100

    Add project files.

---
+++ Fuvar/Fuvar.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fuvar
+{
+    class Fuvar
+    {
+        private int taxi_id;
+        private DateTime indulas;
+        private int idotartam;
+        private double tavolsag;
+        private double viteldij;
+        private double borravalo;
+        private string fizetes_modja;
+
+        public int Taxi_id { get => taxi_id; set => taxi_id = value; }
+        public DateTime Indulas { get => indulas; set => indulas = value; }
+        public int Idotartam { get => idotartam; set => idotartam = value; }
+        public double Tavolsag { get => tavolsag; set => tavolsag = value; }
+        public double Viteldij { get => viteldij; set => viteldij = value; }
+        public double Borravalo { get => borravalo; set => borravalo = value; }
+        public string Fizetes_modja { get => fizetes_modja; set => fizetes_modja = value; }
+
+        public Fuvar(int taxi_id, DateTime indulas, int idotartam, double tavolsag, double viteldij, double borravalo, string fizetes_modja)
+        {
+            this.taxi_id = taxi_id;
+            this.indulas = indulas;
+            this.idotartam = idotartam;
+            this.tavolsag = tavolsag;
+            this.viteldij = viteldij;
+            this.borravalo = borravalo;
+            this.fizetes_modja = fizetes_modja;
+        }
+    }
+}
---
+++ Fuvar/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Fuvar
+{
+    class Program
+    {
+        static List<Fuvar> fuvarok;
+        static void Main(string[] args)
+        {
+            Beolvas();
+            Feladat3();
+            Feladat4(6185);
+            Feladat5();
+            Feladat6();
+            Feladat7();
+            Feladat8();
+
+            Console.WriteLine("program vége");
+            Console.ReadKey();
+        }
+
+        private static void Feladat8()
+        {
+            
+        }
+
+        private static void Feladat7()
+        {
+            int leghosszabb_fuvar = fuvarok[0].Idotartam;
+            int index = 0;
+            for (int i = 0; i < fuvarok.Count; i++)
+            {
+                if (fuvarok[i].Idotartam > leghosszabb_fuvar)
+                {
+                    leghosszabb_fuvar = fuvarok[i].Idotartam;
+                    index = i;
+                }
+            }
+
+            Console.WriteLine($"7. Feladat: Leghosszabb fuvar:\n\tFuvar hossza: {fuvarok[index].Idotartam} másodperc" +
+                $"\n\tTaxi azonosító: {fuvarok[index].Taxi_id}\n\tMegtett távolság: {fuvarok[index].Tavolsag * 1.6} km\n\tViteldíj: {fuvarok[index].Viteldij}$");
+        }
+
+        private static void Feladat6()
+        {
+            double ossz_km = 0;
+            foreach (var fuvar in fuvarok)
+            {
+                ossz_km += fuvar.Tavolsag;
+            }
+
+            Console.WriteLine($"6. feladat: {Math.Round(ossz_km * 1.6, 2)}km");
+        }
+
+        private static void Feladat5()
+        {
+            Console.WriteLine($"5. feladat: ");
+        }
+
+        private static void Feladat4(int taxi_id)
+        {
+            double bevetel = 0;
+            int fuvarok_szama = 0;
+            foreach (var fuvar in fuvarok)
+            {
+                if (fuvar.Taxi_id == taxi_id)
+                {
+                    bevetel += fuvar.Borravalo + fuvar.Viteldij;
+                    fuvarok_szama++; 
+                }
+            }
+
+            Console.WriteLine($"4. feladat: {fuvarok_szama} alatt: {bevetel}$");
+        }
+
+        private static void Feladat3()
+        {
+            Console.WriteLine($"3. Feladat: {fuvarok.Count()} fuvar");
+        }
+
+        private static void Beolvas()
+        {
+            fuvarok = new List<Fuvar>();
+            using (StreamReader sr = new StreamReader("fuvar.csv"))
+            {
+                sr.ReadLine();
+                while (!sr.EndOfStream)
+                {
+                    string[] sor = sr.ReadLine().Split(';');
+                    int taxi_id = int.Parse(sor[0]);
+                    DateTime indulas = DateTime.Parse(sor[1]);
+                    int idotartam = int.Parse(sor[2]);
+                    double tavolsag = double.Parse(sor[3]);
+                    double viteldij = double.Parse(sor[4]);
+                    double borravalo = double.Parse(sor[5]);
+                    string fizetes_modja = sor[6];
+
+                    Fuvar fuvar = new Fuvar(taxi_id, indulas, idotartam, tavolsag, viteldij, borravalo, fizetes_modja);
+                    fuvarok.Add(fuvar);
+                }
+            }
+        }
+    }
+}
